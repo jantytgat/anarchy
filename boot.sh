6660d1758252825a25c85d2682fabd77790c31b3
Jan Tytgat <jan.tytgat@corelayer.eu>
2025-09-24 08:44:40 +0200

    Update boot script
    Signed-off-by: Jan Tytgat <jan.tytgat@corelayer.eu>

--- boot.sh
+++ boot.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
-pacman -S --noconfirm git
-arch-chroot /mnt mkdir -p /root/anarchy
-arch-chroot /mnt git clone git@github.com:jantytgat/anarchy.git /root/anarchy
-arch-chroot /mnt bash /root/anarchy/install/install.sh
+pacman -Sy --noconfirm git
+git clone git@github.com:jantytgat/anarchy.git
+bash anarchy/install/install.sh
