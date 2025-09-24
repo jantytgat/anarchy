e8801f22e88af607edb4b8689fd714d15aae4b82
Jan Tytgat <jan.tytgat@corelayer.eu>
2025-09-24 09:25:15 +0200

    Change git clone to HTTPS
    Signed-off-by: Jan Tytgat <jan.tytgat@corelayer.eu>

--- boot.sh
+++ boot.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 pacman-key --init
 reflector
+pacman -Sy --noconfirm ca-certificates
 pacman -Sy --noconfirm git
-git clone git@github.com:jantytgat/anarchy.git
+git clone https://github.com/jantytgat/anarchy.git
 bash anarchy/install/install.sh
