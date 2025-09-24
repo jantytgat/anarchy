567f8f127500794f088e92fdf02870e953aeb897
Jan Tytgat <jan.tytgat@corelayer.eu>
2025-09-24 13:21:56 +0200

    Remove print_title
    Signed-off-by: Jan Tytgat <jan.tytgat@corelayer.eu>

--- boot.sh
+++ boot.sh
@@ -6,7 +6,6 @@ pacman-key --refresh-keys
 pacman -Sy --noconfirm --quiet git gum
 sleep 5
 
-print_title
 git clone https://github.com/jantytgat/anarchy.git
 cd anarchy/install
 bash install.sh $passphrase
