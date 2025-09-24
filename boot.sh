56979577c529c0f073a5b2876a8810ed8c766990
Jan Tytgat <jan.tytgat@corelayer.eu>
2025-09-24 21:07:51 +0200

    Update boot.sh
    Signed-off-by: Jan Tytgat <jan.tytgat@corelayer.eu>

--- boot.sh
+++ boot.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env bash
-passphrase=$1
 reflector
 pacman-key --init
 pacman-key --refresh-keys
 pacman -Sy --noconfirm --quiet git gum
-sleep 5
 
 git clone https://github.com/jantytgat/anarchy.git
